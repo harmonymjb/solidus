5768c53cb85aef2c074c6051a3f9218364016d71
Harmony Bouvier <harmony@super.gd>
2024-12-03 11:53:53 -0800

    Utilize variant tax category when calculating taxes if possible
    In the default tax_calculator, this change ensures that the variant’s
    current tax_category is considered when calculating taxes.
    
    In the case of the item_rates method, only line_item items will have
    this method, so it is accessed with `try` first, with a fallback to
    without `variant_`
    
    Co-authored-by: Adam Mueller <adam@super.gd>

--- core/app/models/spree/tax/tax_helpers.rb
+++ core/app/models/spree/tax/tax_helpers.rb
@@ -9,7 +9,7 @@ module Spree
         @rates_for_item ||= Spree::TaxRate.item_level.for_address(item.order.tax_address)
 
         @rates_for_item.select do |rate|
-          rate.active? && rate.tax_categories.map(&:id).include?(item.tax_category_id)
+          rate.active? && rate.tax_categories.map(&:id).include?(item.try(:variant_tax_category_id) || item.tax_category_id)
         end
       end
     end
